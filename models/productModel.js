2333c8171bf75e5be7ca7dff81ea0fa5e34fee7a
Gabriel Matos <141865582+gabriellmatos@users.noreply.github.com>
2026-01-15 18:32:00 -0300

    Node.js REST API

---
+++ models/productModel.js
@@ -0,0 +1,19 @@
+const products = require('../data/products');
+
+function findAll() {
+	return new Promise((resolve, reject) => {
+		resolve(products);
+	});
+}
+
+function findById(id) {
+	return new Promise((resolve, reject) => {
+		const product = products.find((product) => product.id === id);
+		resolve(product);
+	});
+}
+
+module.exports = {
+	findAll,
+	findById,
+};
